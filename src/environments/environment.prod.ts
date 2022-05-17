9b142de689eb9e87aba06e597fd92eee55c254f4
Juan Manuel Rosas Salazar <licjuanma@hotmail.com>
2022-05-17 00:41:43 -0600

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,15 @@
 export const environment = {
-  production: true
+  production: true,
+  FP_SERVER_PORT: 'https://trueomni-api.azurewebsites.net/domain',
+  envName: 'dev'
 };
+
+/*
+ * For easier debugging in development mode, you can import the following file
+ * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
+ *
+ * This import should be commented out in production mode because it will have a negative impact
+ * on performance if an error is thrown.
+ */
+// import 'zone.js/plugins/zone-error';  // Included with Angular CLI.
+export const COOKIE_DOMAIN = 'localhost';
